0a4ac26dad0756389b040efe2deecaa9b7a9c580
rama1 <maurya.rama1@google.com>
2024-08-10 11:13:19 +0000

    vm1

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-
 const http = require('http');
 const PORT = process.env.PORT || 9000;
 let slideMode = false;
@@ -6,7 +5,7 @@ let slideMode = false;
 http
   .createServer((req, res) => {
     console.log('New connection');
-    
+
     if (req.url === '/slide') {
       // Toggle slide mode
       slideMode = !slideMode;
@@ -14,8 +13,33 @@ http
     } else {
       // Normal mode
       const message = slideMode
-        ? `<h1 style="color: blue; font-size: 20px; font-weight: bold; color: blue; text-shadow: 2px 2px yellow;">TEAMCOMPUTERS-V2 (Slide Mode)</h1>`
-        : `<h1 style="color: blue; font-size: 20px; font-weight: bold; color: blue; text-shadow: 2px 2px yellow;">WelCome to TeamComputers at delhi office-</h1>`;
+        ? `<h1 style="color: blue; font-size: 20px; font-weight: bold; text-shadow: 2px 2px yellow;">TEAMCOMPUTERS-V2 (Slide Mode)</h1>`
+        : `<html>
+              <head>
+                <style>
+                  body {
+                    display: flex;
+                    justify-content: center;
+                    align-items: center;
+                    height: 100vh;
+                    margin: 0;
+                    background-color: #f0f0f0;
+                    font-family: Arial, sans-serif;
+                  }
+                  h1 {
+                    color: blue;
+                    font-size: 20px;
+                    font-weight: bold;
+                    text-shadow: 2px 2px yellow;
+                    text-align: center;
+                  }
+                </style>
+              </head>
+              <body>
+                <h1>Welcome to TeamComputers at Delhi office-</h1>
+              </body>
+            </html>`;
+      res.writeHead(200, {'Content-Type': 'text/html'});
       res.end(message);
     }
   })
